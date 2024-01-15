ef5992aa8145c6e69e470061b849dc17885a8391
BiagioCaputo <scebiagio98@gmail.com>
2024-01-15 15:32:05 +0100

    creato l'array di oggetti e stilata una versione provvisoria fatta con bootstrap che verrà poi sostituita da javascript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("Js Ok")
+
+//Creo l'array di oggetti 
+
+const teamMembers = [
+    { name: 'Wayne Barnett', role: 'Founder & CEO', memberImg: "wayne-barnett-founder-ceo.jpg"},
+    { name: 'Angela Caroll', role: 'Chief Editor', memberImg: "angela-caroll-chief-editor.jpg"},
+    { name: 'Walter Gordon', role: 'Office Manager', memberImg: "walter-gordon-office-manager.jpg"},
+    { name: 'Angela Lopez', role: 'Social Media Manager', memberImg: "angela-lopez-social-media-manager.jpg"},
+    { name: 'Scott Estrada', role: 'Developer', memberImg: "scott-estrada-developer.jpg"},
+    { name: 'Barbara Ramos', role: 'Graphic Designer ', memberImg: "barbara-ramos-graphic-designer.jpg"}
+]
+
+console.log(teamMembers);
